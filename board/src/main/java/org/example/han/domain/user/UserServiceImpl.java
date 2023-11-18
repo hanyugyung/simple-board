b75e68e08b9f36abee9f0735ade6ff8683f957aa
han60 <dbruddl20202@naver.com>
2023-11-19 01:49:43 +0900

    유저 서비스 클래스 초안

---
+++ board/src/main/java/org/example/han/domain/user/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package org.example.han.domain.user;
+
+public class UserServiceImpl implements UserService {
+
+
+    @Override
+    public long signUp(UserDomainDto.UserSignUpCommand dto) {
+        return 0;
+    }
+
+    @Override
+    public String login(UserDomainDto.UserLoginCommand dto) {
+        return null;
+    }
+}
